b10114848f6cceaff2c7954a30cc08719deab85a
npavangouda <npavangouda@gmail.com>
2018-12-19 12:45:39 +0530

    corrected build error

--- src/app/services/ai.service.ts
+++ src/app/services/ai.service.ts
@@ -17,6 +17,7 @@ export class AiService {
     async predict(imgData: ImageData) {
         const predTf = await tf.tidy(() => {
             let img = tf.fromPixels(imgData, 1);
+            // @ts-ignore
             img = img.reshape([1, 28, 28, 1]);
             img = tf.cast(img, 'float32');
 
